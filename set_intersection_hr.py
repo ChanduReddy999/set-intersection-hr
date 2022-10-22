81cfb9f3597f6a8055cbbb2e42c8d23afd354900
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-10-22 22:52:18 +0530

    Add files via upload

---
+++ set_intersection_hr.py
@@ -0,0 +1,11 @@
+"""set intersection() operation using python (HackeRank Problem)"""
+
+# n=int(input())
+# l=list(input().split())
+# m=int(input())
+# k=list(input().split())
+
+# s1=set(l)
+# s2=set(k)
+
+# print(len(s1.intersection(s2)))
